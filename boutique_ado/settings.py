37cb9286ce199e92ab19cd846e1b4d9b7eecd44a
Demetris Papakyriacou <dpapakyriacou14@gmail.com>
2024-12-24 13:46:42 +0000

    feat: complete checkout form

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "allauth",
     "allauth.account",
     "allauth.socialaccount",
+    'crispy_forms',
     "home",
     "products",
     "bag",
@@ -60,7 +61,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "boutique_ado.urls"
 
-CRISPY_TEMPLATE_PACK = "bootstrap4"
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
