1b2fe1b7551441a72c03b7ca0dffcca0c684139d
Demetris Papakyriacou <dpapakyriacou14@gmail.com>
2025-01-08 16:59:43 +0000

    feat: add cache control

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -181,6 +181,12 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 if ENVIRONMENT == "PROD":
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket config
     AWS_STORAGE_BUCKET_NAME = "boutique-ado-dp-d9499616d598"
     AWS_S3_REGION_NAME = "eu-north-1"
